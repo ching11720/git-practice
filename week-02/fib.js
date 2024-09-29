245ef23d7e7257b8a4ed0b96853eb891fdaed03b
ching11720 <jasmine910402614@gmail.com>
2024-09-29 16:06:53 +0800

    edited format by prettier

--- week-02/fib.js
+++ week-02/fib.js
@@ -1,11 +1,10 @@
 function fib(n) {
-    if (n==0) 
-        return 0;
-    if (n==1||n==2)
-        return 1;
-    
-    let fib_1=1, fib_2=1;
-    for (let i=2; i<n; ++i) {
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+
+    let fib_1 = 1,
+        fib_2 = 1;
+    for (let i = 2; i < n; ++i) {
         temp = fib_1 + fib_2;
         fib_1 = fib_2;
         fib_2 = temp;
