7ed1b22d1ca9c417cd377c50af2015631a08ec4c
ching11720 <jasmine910402614@gmail.com>
2024-09-29 15:56:36 +0800

    edited format by prettier

--- week-02/sum.js
+++ week-02/sum.js
@@ -1,21 +1,20 @@
-function sum_foreach(ary) {
-	let sum=0;
-    ary.forEach((x) => sum+=x);
+function sumForeach(ary) {
+    let sum = 0;
+    ary.forEach((x) => (sum += x));
     return sum;
 }
 
-function sum_reduce(ary) {
-    let sum=0;
+function sumReduce(ary) {
+    let sum = 0;
     return ary.reduce((accumulator, current) => accumulator + current, sum);
 }
 
-function sum_sigma(n) {
-    if (n==1)
-        return 1;
-    return n+sum_sigma(n-1);
+function sumSigma(n) {
+    if (n == 1) return 1;
+    return n + sumSigma(n - 1);
 }
 
-console.log('sum by foreach: '+sum_foreach([1, 5, 3, 2])); // 11
-console.log('sum by reduce: '+sum_reduce([1, 5, 3, 2])); // 11
-console.log('sum by sigma: '+sum_sigma(5)); // 15
+console.log('sum by foreach: ' + sumForeach([1, 5, 3, 2])); // 11
+console.log('sum by reduce: ' + sumReduce([1, 5, 3, 2])); // 11
+console.log('sum by sigma: ' + sumSigma(5)); // 15
 // 各種加總參考：https://blog.yowko.com/javascript-array-sum/
